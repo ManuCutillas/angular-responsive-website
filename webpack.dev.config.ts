bfd66cda1d35f1a2bc387b95cb4eb3f2f4ea8f46
manucutillas <manucutillas@outlook.com>
2017-02-10 20:40:54 +0100

    First commit

---
+++ webpack.dev.config.ts
@@ -0,0 +1,168 @@
+const webpack = require('webpack');
+const path = require('path');
+const clone = require('js.clone');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const webpackMerge = require('webpack-merge');
+
+//SOURCE PATHS
+const PROD_CLIENT_DIR = 'dist/client';
+const SRC_CLIENT_DIR = 'src/client';
+const FILES_SRC = `${SRC_CLIENT_DIR}/www`;
+const SRC_SERVER_BIN = 'src/server/bin';
+const PROD_SERVER_BIN = 'dist/server/bin';
+
+export const commonPlugins = [
+  new CopyWebpackPlugin([
+    {
+      from: path.resolve(__dirname, `${FILES_SRC}/index.html`),
+      to: path.resolve(__dirname, PROD_CLIENT_DIR)
+    },
+    {
+      from: path.resolve(__dirname, `${FILES_SRC}/assets`),
+      to: path.resolve(__dirname, `${PROD_CLIENT_DIR}/assets`)
+    },
+    {
+      from: path.resolve(__dirname, `${FILES_SRC}/styles`),
+      to: path.resolve(__dirname, `${PROD_CLIENT_DIR}/styles`)
+    },
+    {
+      from: path.resolve(__dirname, `${SRC_SERVER_BIN}`),
+      to: path.resolve(__dirname, `${PROD_SERVER_BIN}`)
+    }
+    ]),
+  new webpack.ContextReplacementPlugin(
+    // The (\\|\/) piece accounts for path separators in *nix and Windows
+    /angular(\\|\/)core(\\|\/)src(\\|\/)linker/,
+    root('./src'),
+    {
+      // your Angular Async Route paths relative to this root directory
+    }
+  ),
+
+  // Loader options
+  new webpack.LoaderOptionsPlugin({
+
+  }),
+
+];
+export const commonConfig = {
+  // https://webpack.github.io/docs/configuration.html#devtool
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.ts', '.js', '.json'],
+    modules: [ root('node_modules') ]
+  },
+  context: __dirname,
+  output: {
+    publicPath: '',
+    filename: '[name].bundle.js'
+  },
+  module: {
+    rules: [
+       { 
+         test: /\.js$/,
+         exclude: /(node_modules|bower_components)/,
+         loader: 'babel-loader',
+         query: {
+           presets: ['es2015']
+         }
+      },
+      // TypeScript
+      { test: /\.ts$/,   use: ['awesome-typescript-loader', 'angular2-template-loader'] },
+      { test: /\.html$/, use: 'raw-loader' },
+      { test: /\.css$/,  use: 'raw-loader' },
+      { test: /\.json$/, use: 'json-loader' }
+    ],
+  },
+  plugins: [
+    // Use commonPlugins.
+  ]
+
+};
+
+// Client.
+export var clientPlugins = [
+
+];
+export var clientConfig = {
+  target: 'web',
+  entry: './src/client/main.jit',
+  output: {
+    path: root('dist/client')
+  },
+  node: {
+    global: true,
+    crypto: 'empty',
+    __dirname: true,
+    __filename: true,
+    process: true,
+    Buffer: false
+  }
+};
+
+
+// Server.
+export var serverPlugins = [
+
+];
+
+export var serverConfig = {
+  target: 'node',
+  entry: './src/server', // use the entry file of the node server if everything is ts rather than es5
+  output: {
+    filename: 'index.js',
+    path: root('dist/server'),
+    libraryTarget: 'commonjs2'
+  },
+  module: {
+    rules: [
+      { test: /@angular(\\|\/)material/, use: "imports-loader?window=>global" }
+    ],
+  },
+  externals: includeClientPackages(
+    /@angularclass|@angular|angular2-|ng2-|ng-|@ng-|angular-|@ngrx|ngrx-|@angular2|ionic|@ionic|-angular2|-ng2|-ng/
+  ),
+  node: {
+    global: true,
+    crypto: true,
+    __dirname: true,
+    __filename: true,
+    process: true,
+    Buffer: true
+  }
+};
+
+export default [
+  // Client
+  webpackMerge(clone(commonConfig), clientConfig, { plugins: clientPlugins.concat(commonPlugins) }),
+
+  // Server
+  webpackMerge(clone(commonConfig), serverConfig, { plugins: serverPlugins.concat(commonPlugins) })
+];
+
+
+
+
+// Helpers
+export function includeClientPackages(packages, localModule?: string[]) {
+  return function(context, request, cb) {
+    if (localModule instanceof RegExp && localModule.test(request)) {
+      return cb();
+    }
+    if (packages instanceof RegExp && packages.test(request)) {
+      return cb();
+    }
+    if (Array.isArray(packages) && packages.indexOf(request) !== -1) {
+      return cb();
+    }
+    if (!path.isAbsolute(request) && request.charAt(0) !== '.') {
+      return cb(null, 'commonjs ' + request);
+    }
+    return cb();
+  };
+}
+
+export function root(args) {
+  args = Array.prototype.slice.call(arguments, 0);
+  return path.join.apply(path, [__dirname].concat(args));
+}
